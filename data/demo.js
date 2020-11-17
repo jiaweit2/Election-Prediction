526570a42ebac2fc9f4d8c016685a840eb4e5224
jiaweit2 <jiaweit2@illinois.edu>
2020-11-17 15:11:31 -0600

    update

---
+++ data/demo.js
@@ -0,0 +1,64 @@
+function createDemoGraph(state) {
+    var ctx = document.getElementById('chart-demo').getContext('2d');
+    var config = {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                ],
+                backgroundColor: [
+                    window.chartColors.red,
+                    window.chartColors.orange,
+                    window.chartColors.yellow,
+                    window.chartColors.green,
+                    window.chartColors.blue,
+                ],
+                label: 'Actual'
+            }, {
+                data: [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                ],
+                backgroundColor: [
+                    window.chartColors.red,
+                    window.chartColors.orange,
+                    window.chartColors.yellow,
+                    window.chartColors.green,
+                    window.chartColors.blue,
+                ],
+                label: 'Predicted'
+            }],
+            labels: [
+                'Red',
+                'Orange',
+                'Yellow',
+                'Green',
+                'Blue'
+            ]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Voter Demographics'
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
+    };
+    Chart.defaults.font.color = "#000";
+    window.charts.push(new Chart(ctx, config));
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const randomScalingFactor = function () {
     return Math.round(Math.random() * 100);
 };
+window.charts = []
 
 function showResults(state, bg) {
     document.getElementById("popup").style.visibility = "visible";
@@ -10,72 +11,15 @@ function showResults(state, bg) {
         bg = "#EEAEAFE0";
     }
     document.getElementById("popup").style.backgroundColor = bg;
-    var ctx = document.getElementById('chart-area').getContext('2d');
-    var config = {
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: [
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                ],
-                backgroundColor: [
-                    window.chartColors.red,
-                    window.chartColors.orange,
-                    window.chartColors.yellow,
-                    window.chartColors.green,
-                    window.chartColors.blue,
-                ],
-                label: 'Actual'
-            }, {
-                data: [
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                    randomScalingFactor(),
-                ],
-                backgroundColor: [
-                    window.chartColors.red,
-                    window.chartColors.orange,
-                    window.chartColors.yellow,
-                    window.chartColors.green,
-                    window.chartColors.blue,
-                ],
-                label: 'Predicted'
-            }],
-            labels: [
-                'Red',
-                'Orange',
-                'Yellow',
-                'Green',
-                'Blue'
-            ]
-        },
-        options: {
-            responsive: true,
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Voter Turnout rates'
-            },
-            animation: {
-                animateScale: true,
-                animateRotate: true
-            }
-        }
-    };
-    Chart.defaults.font.color = "#000";
-    window.chart = new Chart(ctx, config);
+    createTurnout(state);
+    createDemoGraph(state);
 }
 
 function closePopup() {
     document.getElementById("popup").style.visibility = "hidden";
     document.getElementById("popup").style.backgroundColor = "#ffffff00";
-    window.chart.destroy();
+    window.charts.forEach(chart => {
+        chart.destroy();
+    });
+    window.charts = []
 }
